344484ef13c190f24085cd2f93a31f576c008d49
mk <mkao006@gmail.com>
2016-03-17 21:31:39 +0800

    first commit

---
+++ beanstalk/random-walker/random_walker/user_action/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender_choice = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('', 'Unspecified'),
+    )
+    gender = models.CharField(max_length=2, choices = gender_choice, default='')
