d447618d09e40a8a5b96e1880c3a2e1e8578cf5e
mk <mkao006@gmail.com>
2016-03-18 16:16:14 +0800

    added configuration for serving static files on AWS

--- beanstalk/random-walker/random_walker/random_walker/settings.py
+++ beanstalk/random-walker/random_walker/random_walker/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = (
     'random_walker',
     'user_action',
     'django_mobile',
+    'storages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -142,9 +143,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+AWS_STORAGE_BUCKET_NAME = 'BUCKET_NAME'
+AWS_ACCESS_KEY_ID = 'AKIAIYVVLKL24VQACW4A'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in
+# S3 storage, keep it simple - just use this domain plus the path. (If
+# this isn't set, things get complicated). This controls how the
+# `static` template tag from `staticfiles` gets expanded, if you're
+# using it.
+
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're
+# using that. Or if anything else refers directly to STATIC_URL. So
+# it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the
+# collected static files (when you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 LOGIN_URL = os.path.join(BASE_DIR, 'registration/login_view/')
