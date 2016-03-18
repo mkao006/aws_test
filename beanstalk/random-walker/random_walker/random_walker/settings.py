3358376c0eb80c41291c9ceea015ffa8388c1ca5
mk <mkao006@gmail.com>
2016-03-18 11:30:14 +0800

    removed static files directory and use static url to collect from path

--- beanstalk/random-walker/random_walker/random_walker/settings.py
+++ beanstalk/random-walker/random_walker/random_walker/settings.py
@@ -145,6 +145,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_URL = os.path.join(BASE_DIR, 'registration/login_view/')
