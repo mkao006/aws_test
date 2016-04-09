06c992cb48bdda388269c8cd7bf698dc4f62a9a7
mk <mkao006@gmail.com>
2016-04-09 11:58:54 +0200

    added switch to the static file depending on the deployment environment

--- beanstalk/random-walker/random_walker/random_walker/settings.py
+++ beanstalk/random-walker/random_walker/random_walker/settings.py
@@ -151,7 +151,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
+
 
 # Tell django-storages that when coming up with the URL for an item in
 # S3 storage, keep it simple - just use this domain plus the path. (If
@@ -162,13 +162,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # We also use it in the next setting.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# This is used by the `static` template tag from `static`, if you're
-# using that. Or if anything else refers directly to STATIC_URL. So
-# it's safest to always set it.
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-
 # Tell the staticfiles app to use S3Boto storage when writing the
 # collected static files (when you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 LOGIN_URL = os.path.join(BASE_DIR, 'registration/login_view/')
+
+if socket.gethostname() == 'mk-IdeaPad-U330p':
+    STATIC_URL = '/static/'
+else:
+    # This is used by the `static` template tag from `static`, if you're
+    # using that. Or if anything else refers directly to STATIC_URL. So
+    # it's safest to always set it.
+    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
